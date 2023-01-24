f029198e732ca6ffbedb2e6728b4d9e3c02268f4
AzimAhmedBijapur <azeembijapur786@gmail.com>
2023-01-24 18:18:33 +0530

    footer added

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const Library = [];
+// const Library = [];
 
-function Book(title, author, pages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-}
+// function Book(title, author, pages, isRead) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.isRead = isRead;
+// }
 
-function addBookToLibrary() {
-  const title = 'jungle book';
-  const author = 'j k rowling';
-  const pages = 200;
-  const isRead = true;
-  const book = new Book(title, author, pages, isRead);
-  Library.push(book);
-  console.log(Library);
-}
+// function addBookToLibrary() {
+//   const title = 'jungle book';
+//   const author = 'j k rowling';
+//   const pages = 200;
+//   const isRead = true;
+//   const book = new Book(title, author, pages, isRead);
+//   Library.push(book);
+//   console.log(Library);
+// }
